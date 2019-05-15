d99f5d7901240b004fa0722c138931313ad69939
Jan Aerts <jan.aerts@gmail.com>
2019-05-15 15:58:40 +0200

    Removed unnecessary libraries from project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject stad "0.1.0"
   :description "Simplified Topological Approximation of Data"
+  :url "http://github.com/vda-lab/clj-stad"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.csv "0.1.4"]
                  [net.mikera/core.matrix "0.62.0"]
@@ -10,8 +11,6 @@
                  [org.clojure/tools.logging "0.4.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [incanter/incanter "1.9.3"]
-                 [incanter-gorilla "0.1.0"]
-                 [progrock "0.1.2"]
                  [org.clojure/core.async "0.4.474"]]
   :main ^:skip-aot gorilla-test.core
   :target-path "target/%s"
